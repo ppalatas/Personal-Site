d0c1bc2b50c5fc9ef6ba9b4839ac6aad17166b7e
Phillip Palatas <PhillipPalatas@Phillips-MacBook-Pro-3.local>
2017-03-10 02:10:08 -0600

    Added hover features and notes for next steps

--- js/index.js
+++ js/index.js
@@ -1,5 +1,30 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 
+	$("#header > ul > li.name").hover(function(){
+		$("#header > ul > li.name").addClass("headerHover");
+	}, function(){
+		$("#header > ul > li.name").removeClass("headerHover");
+	});
+
+	$("#header > ul > a:nth-child(2) > li").hover(function(){
+		$("#header > ul > a:nth-child(2) > li").addClass("headerHover");
+	}, function (){
+
+		$("#header > ul > a:nth-child(2) > li").removeClass("headerHover");
+
+	});
+
+	$("#header > ul > a:nth-child(3) > li").hover(function(){
+		$("#header > ul > a:nth-child(3) > li").addClass("headerHover");
+	}, function(){
+		$("#header > ul > a:nth-child(3) > li").removeClass("headerHover");
+	});
+
+	$("#header > ul > li.contact").hover(function(){
+		$("#header > ul > li.contact").addClass("headerHover");
+	}, function(){
+		$("#header > ul > li.contact").removeClass("headerHover");
+	});
 
 	$("#column1").hover(function(){
 		$(".column1Heading").addClass("headerChange");
@@ -21,37 +46,7 @@
 	}, function(){
 		$(".column3Heading").removeClass("headerChange");
 	});
+});
 
 	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
